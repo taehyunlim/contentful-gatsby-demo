a926926079f55dd107ad180e50669b6ef5f36da9
taehyunlim <taehyun.lim@gmail.com>
2020-05-10 22:32:01 -0700

    adds index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const BlogPost = ({ node }) => {
+  return (
+    <li>
+      <Link to={node.slug}>{node.title}</Link>
+    </li>
+  )
+}
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Blog Post from Contentful</h1>
+    <ul>
+      {data.allContentfulBlog.edges.map(edge => (
+        <BlogPost node={edge.node} />
+      ))}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query pageQuery {
+    allContentfulBlog(filter: { node_locale: { eq: "en-US" } }) {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+  }
+`
